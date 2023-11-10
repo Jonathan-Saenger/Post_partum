05c8c09af8b783099952777313355235359071ad
Jonathan-Saenger <jonathan.saenger@outlook.fr>
2023-11-11 00:18:36 +0100

    création des requêtes sql

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE postpartum ; 
+
+CREATE TABLE Admin ( 
+    id INT AUTO_INCREMENT NOT NULL,
+    nom VARCHAR(50) NOT NULL,
+    prenom VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+    );
