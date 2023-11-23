a2826a7aa496490c2f81c45fe3abf63eab398a66
Jonathan-Saenger <jonathan.saenger@outlook.fr>
2023-11-24 00:59:38 +0100

    création des CRUDs

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,12 +3,16 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Blog;
+use App\Entity\Post;
+use App\Entity\User;
 use App\Entity\Evenement;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use App\Entity\Commentaire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -28,6 +32,9 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Blog', 'fas fa-list', Blog::class);
+        yield MenuItem::linkToCrud('Post', 'fas fa-list', Post::class);  
+        yield MenuItem::linkToCrud('Commentaire', 'fas fa-list', Commentaire::class);
         yield MenuItem::linkToCrud('Evenement', 'fas fa-list', Evenement::class);
+        yield MenuItem::linkToCrud('User', 'fas fa-list', User::class);   
     }
 }
