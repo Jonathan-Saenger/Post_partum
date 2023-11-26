556618d81bc28979e34d7d6e43526a5485fbf969
Jonathan-Saenger <jonathan.saenger@outlook.fr>
2023-11-26 01:19:01 +0100

    installation de rate-limiter & configuration de la récupération des post dans la BDD

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +12,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class PostController extends AbstractController
 {
     #[Route('/post', name: 'app_post')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine, PostRepository $PostRepository): Response
     {
+        $PostRepository = $doctrine->getRepository(Post::class);
+        $Post = $PostRepository->findAll();
+
         return $this->render('post.html.twig', [
             'controller_name' => 'PostController',
+            'Post' => $Post,
         ]);
     }
 }
