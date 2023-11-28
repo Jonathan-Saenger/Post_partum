f26f92dee0106f1730f606a5ba0d12e36e0da883
Jonathan-Saenger <jonathan.saenger@outlook.fr>
2023-11-28 21:50:14 +0100

    style de la page blog

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
     #[Route('/blog', name: 'app_blog')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine, PostRepository $PostRepository): Response
     {
+        $PostRepository = $doctrine->getRepository(Post::class);
+        $Post = $PostRepository->findAll();
+
         return $this->render('blog.html.twig', [
             'controller_name' => 'BlogController',
+            'Post' => $Post
         ]);
     }
 }
--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -5,20 +5,21 @@ namespace App\Controller;
 use App\Entity\Post;
 use App\Repository\PostRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PostController extends AbstractController
 {
-    #[Route('/post', name: 'app_post')]
-    public function index(ManagerRegistry $doctrine, PostRepository $PostRepository): Response
+    #[Route('/post/{id}', name: 'app_post')]
+    public function index($id, ManagerRegistry $doctrine, PostRepository $PostRepository, 
+    Post $Post): Response
     {
-        $PostRepository = $doctrine->getRepository(Post::class);
-        $Post = $PostRepository->findAll();
+            $PostRepository = $doctrine ->getRepository(Post::class);
+            $Post = $PostRepository ->find($id);
 
         return $this->render('post.html.twig', [
-            'controller_name' => 'PostController',
+            'controller_name' => 'BlogController',
             'Post' => $Post,
         ]);
     }
