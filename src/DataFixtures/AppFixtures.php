b2dfa00dd376e5cfe7f355bfae999ab6b3c4d97f
Jonathan-Saenger <jonathan.saenger@outlook.fr>
2023-12-01 00:09:26 +0100

    test fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\Post;
+use Faker\Generator;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    /**
+     * @var Generator 
+     */
+    private Generator $faker; 
+
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        //Création de 10 posts
+        for ($i = 0; $i < 10; $i++) {
+            $post = new Post();
+            $post->setCategorie($this->faker->randomElement(['Allaitement','DMAE', 'Grossesse']))
+                ->setTitre($this->faker->word(1, 5))
+                ->setArticle($this->faker->paragraphs(2, true))
+                ->setImage($this->faker->image(null, 360, 360))
+                ->setJourRedaction($this->faker->dateTimeBetween('-5 week', '+1 week'));
+            $manager->persist($post);
+        }
+        $manager->flush();
+    }
+}
