75d70d8c721203906adf1cbfa75faf924d4ddfc4
Jonathan Saenger <jonathan.saenger@outlook.fr>
2023-12-12 01:48:48 +0100

    FRONT HOME > Ajout du bloc Post

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine, PostRepository $PostRepository): Response
     {
+
+        $PostRepository = $doctrine ->getRepository(Post::class);
+        $Post = $PostRepository ->findAll();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'Post'=> $Post,
         ]);
     }
 }
