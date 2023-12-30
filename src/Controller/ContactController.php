54dfd8e8a83dd9e47e4b3331e897ad1338e5add4
Jonathan Saenger <jonathan.saenger@outlook.fr>
2023-12-30 23:09:17 +0100

    F06: CONTACT > Installation et configuration du Bundle MAILER

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,18 +6,30 @@ use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mime\Email;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
         $form = $this->createForm(ContactType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // configurer Mailer
+            $data = $form->getData();
+
+            $email = (new Email())
+                ->from($data['email'])
+                ->to('lea.bernardo91@gmail.com')
+                ->subject('Formulaire de contact')
+                ->html("<p>Bonjour Léa ! Tu as reçu une demande de contact de {$data['prenom']} {$data['nom']} ! Son numéro de téléphone est 
+                le {$data['telephone']}. Voici son message : {$data['message']}</p>");
+                
+            $mailer->send($email);
+
             return $this->redirectToRoute('/submit');
         }
 
