5cc4aba75561f95535992f0bd524c7d3b9bb7ae4
Jonathan Saenger <jonathan.saenger@outlook.fr>
2024-01-07 21:21:10 +0100

    F07: INSCRIPTION > Configuration de la page SUCCESS, REGEX dans le registrationForm, message d'erreur dans le twig

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,21 +4,49 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
+        ->add('nom', TextType::class, [
+            'constraints' => [
+                new Length([
+                    'min' => 2,
+                    'minMessage' => 'Veuillez saisir un minimum de {{ limit }} caractères',
+                    'max' => 15,
+                    'maxMessage' => 'Vous avez dépassé le nombre de caractères limités',
+                ]),
+                new Regex([
+                    'pattern' => '/^[a-zA-Z-]+$/',
+                    'match' => true,
+                    'message' => 'Vous avez employé un caractère non autorisé, merci de le supprimer'
+                ]),
+            ]
+        ])
+        ->add('prenom', TextType::class, [
+            'constraints' => [
+                new Length([
+                    'min' => 5,
+                    'minMessage' => 'Veuillez saisir un minimum de caractère',
+                    'max' => 15,
+                    'maxMessage' => 'Vous avez dépasser le nombre de caractères limités',
+                ]),
+                new Regex([
+                    'pattern' => '/^[a-zA-Z-]+$/',
+                    'match' => true,
+                    'message' => 'Vous avez employé un caractère non autorisé, merci de le supprimer'
+                ])
+            ]
+        ])
             ->add('email')
             ->add('Password', PasswordType::class, [
                                 // instead of being set onto the object directly,
@@ -29,11 +57,9 @@ class RegistrationFormType extends AbstractType
                     new NotBlank([
                         'message' => 'Please enter a password',
                     ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
+                    new Regex([
+                        'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&\/:,])[A-Za-z\d@$!%*?&\/:,]{8,}$/',
+                        'message' => 'Votre Password doit contenir au moins une lettre majuscule, un chiffre, un caractère spécial et 8 caractères.',
                     ]),
                 ],
             ])
