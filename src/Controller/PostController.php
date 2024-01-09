2293a1f4a05a132615e29a7da363de0eeaabb66c
Jonathan Saenger <jonathan.saenger@outlook.fr>
2024-01-09 22:00:25 +0100

    F09: COMMENTAIRE > Création du formulaire de commentaire et configuration du Controller Post avec ajout du formulaire

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,12 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Commentaire;
 use App\Entity\Post;
+use App\Form\CommentType;
 use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class PostController extends AbstractController
 {
@@ -23,4 +27,30 @@ class PostController extends AbstractController
             'Post' => $Post,
         ]);
     }
+
+    #[Route("/comment/{id}", name: "comment")]
+    public function comment(Request $request, Post $post, EntityManagerInterface $em): Response
+    {
+        $comment = new Commentaire();
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+ 
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Set the user and the post for the comment
+            $comment->setUser($this->getUser());
+            $comment->setPost($post);
+ 
+            // Save the comment in the database
+            $em->persist($comment);
+            $em->flush();
+ 
+            return $this->redirectToRoute('app_post', ['id' => $post->getId()]);
+        }
+ 
+        return $this->render('post.html.twig', [
+            'post' => $post,
+            'form' => $form->createView(),
+        ]);
+    }
 }
+
