cb18b14ce57822578bad5fd24b088670c95cf39b
Jonathan Saenger <jonathan.saenger@outlook.fr>
2024-01-10 23:56:18 +0100

    F09: COMMENTAIRE > Correction des relations entre User, Commentaire et Post

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -37,7 +37,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 50)]
     private ?string $prenom = null;
 
-    #[ORM\OneToMany(mappedBy: 'commentaire', targetEntity: Commentaire::class)]
+    #[ORM\OneToMany(mappedBy: 'user', targetEntity: Commentaire::class)]
     private Collection $commentaire;
 
     #[ORM\OneToMany(mappedBy: 'user', targetEntity: Evenement::class)]
@@ -163,7 +163,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         if (!$this->commentaire->contains($commentaire)) {
             $this->commentaire->add($commentaire);
-            $commentaire->setCommentaire($this);
+            $commentaire->setUser($this);
         }
 
         return $this;
@@ -173,8 +173,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         if ($this->commentaire->removeElement($commentaire)) {
             // set the owning side to null (unless already changed)
-            if ($commentaire->getCommentaire() === $this) {
-                $commentaire->setCommentaire(null);
+            if ($commentaire->getUser() === $this) {
+                $commentaire->setUser(null);
             }
         }
 
