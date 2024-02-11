93e3f8b1801fff92c94ce1e7622b9c451a13c289
Jonathan Saenger <jonathan.saenger@outlook.fr>
2024-02-11 21:01:54 +0100

    MAIN > suppression du fichier postController et refactorisation du BlogController

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Commentaire;
+use App\Entity\Post;
+use App\Form\CommentType;
 use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class BlogController extends AbstractController
 {
@@ -16,5 +22,34 @@ class BlogController extends AbstractController
             'Post' => $PostRepository->findBy([], ['jour_redaction' => 'DESC'])
         ]);
     }
+
+    #[Route('/post/{id}', name: 'app_post')]
+    public function article(Request $request, $id, PostRepository $PostRepository, Post $Post, EntityManagerInterface $entityManager): Response
+    {
+        //affichage du formulaire de commentaire
+        $commentaire = new Commentaire();
+        $form = $this->createForm(CommentType::class, $commentaire);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Associer le commentaire à l'utilisateur et au post
+            $commentaire->setUser($this->getUser());
+            $commentaire->setPost($Post);
+            $commentaire->setJourPublication(new \DateTime());
+
+            $entityManager->persist($commentaire);
+            $entityManager->flush();
+
+            return $this->redirect($request->getUri());
+        }
+
+        //Récupération des commentaires 
+        $commentaire = $Post->getCommentaire();
+
+        return $this->render('post.html.twig', [
+            'Post' => $PostRepository ->find($id),
+            'form' => $form->createView(),
+        ]);  
+    } 
 }
 
