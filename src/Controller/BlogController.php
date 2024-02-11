037d3bdb7cf34c9d68c7ff4b5988eb0e167e1444
Jonathan Saenger <jonathan.saenger@outlook.fr>
2024-02-11 20:14:51 +0100

    MAIN > refacto bis repetita

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Post;
 use App\Repository\PostRepository;
-use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,7 +13,7 @@ class BlogController extends AbstractController
     public function index(PostRepository $PostRepository): Response
     {
         return $this->render('blog.html.twig', [
-            'Post' => $Post = $PostRepository->findBy([], ['jour_redaction' => 'DESC'])
+            'Post' => $PostRepository->findBy([], ['jour_redaction' => 'DESC'])
         ]);
     }
 }
