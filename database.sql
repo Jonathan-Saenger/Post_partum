f9bbd509f738ae60af4073b11295e5021811a2f1
Jonathan Saenger <jonathan.saenger@outlook.fr>
2024-02-26 22:19:59 +0100

    MAIN > Correction des requêtes SQL

--- database.sql
+++ database.sql
@@ -6,64 +6,62 @@ CREATE TABLE Admin (
     prenom VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id_admin)
     );
 
-ALTER TABLE Admin 
-    add PRIMARY KEY (Id_admin);
-
 CREATE TABLE Evenement (
-    Id_evenement INT AUTO_INCREMENT NOT NULL, 
+    id_evenement INT AUTO_INCREMENT NOT NULL, 
     titre VARCHAR(50) NOT NULL,
     image longblob DEFAULT NULL, 
     description VARCHAR(500) NOT NULL,
     date_creation DATE DEFAULT NULL,
+    id_admin INT NOT NULL,
+    PRIMARY KEY (id_evenement),
+    FOREIGN KEY (id_admin) REFERENCES Admin(id_admin)
     );
 
-ALTER TABLE Evenement 
-    add PRIMARY KEY (Id_evenement), Id_admin INT NOT NULL, FOREIGN KEY(Id_admin) REFERENCES admin(Id_admin);
-
 CREATE TABLE Blog (
-    Id_blog INT AUTO_INCREMENT NOT NULL,
-    nom VACHAR(50) 
+    id_blog INT AUTO_INCREMENT NOT NULL,
+    nom VARCHAR(50) ,
+    PRIMARY KEY (id_blog)
 );
 
-ALTER TABLE Blog
-    add PRIMARY KEY (Id_blog);
-
 CREATE TABLE Post (
-    Id_post INT AUTO_INCREMENT NOT NULL,
-    catégorie VARCHAR(50) NOT NULL,
+    id_post INT AUTO_INCREMENT NOT NULL,
+    categorie VARCHAR(50) NOT NULL,
     titre VARCHAR(50) NOT NULL,
     article LONGTEXT NOT NULL,
     image longblob DEFAULT NULL,
     jour_rédaction DATE DEFAULT NULL,
+    id_blog INT NOT NULL, 
+    id_admin INT NOT NULL,
+    PRIMARY KEY (id_post),
+    FOREIGN KEY (id_blog) REFERENCES Blog(id_blog),
+    FOREIGN KEY (id_admin) REFERENCES Admin(id_admin)
 );
 
-ALTER TABLE Post 
-    ADD PRIMARY KEY (Id_post), Id_blog INT NOT NULL, FOREIGN KEY(Id_blog) REFERENCES Blog(Id_blog), 
-    Id_admin INT NOT NULL, FOREIGN KEY(Id_Admin) REFERENCES Admin(Id_admin); 
-
-CREATE TABLE "User" (
-    Id_user INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE User (
+    id_user INT AUTO_INCREMENT NOT NULL,
     nom VARCHAR(50) NOT NULL,
-    prénom VARCHAR (50) NOT NULL, 
+    prenom VARCHAR (50) NOT NULL, 
     email VARCHAR (50) NOT NULL,
     password VARCHAR (50) NOT NULL,
+    id_admin INT NOT NULL, 
+    PRIMARY KEY (id_user),
+    FOREIGN KEY (id_admin) REFERENCES Admin(id_admin)
 );
 
-ALTER TABLE "User" 
-    add PRIMARY KEY (id_user), Id_admin INT NOT NULL, FOREIGN KEY(Id_admin) REFERENCES admin(Id_admin); 
-
 CREATE TABLE Commentaire (
-    Id_commentaire INT AUTO_INCREMENT NOT NULL,
+    id_commentaire INT AUTO_INCREMENT NOT NULL,
     nom VARCHAR(50) NOT NULL, 
     réponse LONGTEXT NOT NULL, 
     jour_publication DATE DEFAULT NULL,
+    PRIMARY KEY (id_commentaire), 
+    id_admin INT NOT NULL, 
+    id_post INT NOT NULL, 
+    id_user INT NOT NULL,
+    FOREIGN KEY (id_admin) REFERENCES Admin(id_admin),
+    FOREIGN KEY (id_post) REFERENCES Post(id_post), 
+    FOREIGN KEY (id_user) REFERENCES User(id_user)
 );
-
-ALTER TABLE Commentaire 
-    ADD PRIMARY KEY (id_commentaire), 
-    Id_admin INT NOT NULL, FOREIGN KEY(Id_admin) REFERENCES Admin(Id_admin),
-    Id_post INT NOT NULL, FOREIGN KEY(Id_post) REFERENCES Post(Id_post),
-    Id_user INT NOT NULL, FOREIGN KEY(Id_user) REFERENCES User(Id_user);
 
