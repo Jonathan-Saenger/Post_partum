15f9b79f2ab3726c850937cadafab320a0d97636
Jonathan Saenger <jonathan.saenger@outlook.fr>
2025-03-26 12:18:33 +0100

    MAIN > securité MàJ

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 
 
 class UserCrudController extends AbstractCrudController
@@ -11,6 +13,14 @@ class UserCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return User::class;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        // Supprime l'action de suppression pour l'entité User
+        return parent::configureActions($actions)
+            ->disable(Action::DELETE);
+
     }
 
     /*
